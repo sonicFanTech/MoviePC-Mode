d392ade462142aac88f22a0359a782704ca6d8b4
sonic Fan Tech <sonicfan1348@gmail.com>
2026-06-12 21:54:30 -0600

    Delete Manual-Install directory

--- Manual-Install/07-Enable-AppLocker-AuditOnly.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-net session >nul 2>&1 || (echo ERROR: Run as Administrator.& pause & exit /b 1)
-echo This is AuditOnly mode. It records events but should not block apps.
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Scripts\07-Enable-AppLocker-AuditOnly.ps1"
-pause
--- Manual-Install/08-Emergency-Restore-Explorer.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-net session >nul 2>&1 || (echo ERROR: Run as Administrator.& pause & exit /b 1)
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Scripts\08-Emergency-Restore-Explorer.ps1"
-pause
